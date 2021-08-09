2fb7944446b555a7dce5e21ce4736add509ad504
Quabenaantidote <kwabenaabankwahfrimpong@gmail.com>
2021-08-09 14:35:13 +0000

    initial commit

---
+++ yeeee/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int y;
+   int x;
+   int sum;
+
+   y = 4 ;
+   x = 30 ;
+   sum = x + y ;
+   cout << sum ;
+
+        return 0;
+}
